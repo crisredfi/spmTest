5d3652c674258583660549f19ad5a7b0ef5a393c
aleix guri <aleix.guri@payworks.com>
2022-04-27 13:25:25 +0200

    mpos_Ui added

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "mpos_core",
-            targets: ["mpos_core"]),
+            targets: ["mpos_core", "mpos_ui"]),
     ],
     
     targets: [
